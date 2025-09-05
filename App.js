2242bee3bf496b958a6203b605ed7c4c75d170dd
CaioDalNegro <caiodalnegro@gmail.com>
2025-09-05 19:38:26 -0300

    NEW: inserção e mockup

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState} from "react";
+import { View, Text, ScrollView, StyleSheet, Button } from "react-native";
+import api from './src/devices/api';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Declaração do componente principal 'App'
+export default App(){
+// users - variavel de estado que conterá a lista de usuários
+const [users, setUsers] = useState([]);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/devices/api.js
@@ -0,0 +1,6 @@
+//componente API Axios
+import axios from "axios";
+const api = axios.create({
+    timeout:2000
+})
+export default api;
